1573cc1ba520a82270e60f3c1d38846b72147b5e
Robert Marko <robimarko@gmail.com>
2017-09-26 16:49:16 +0200

    Add RSSILEDs to Deliberant

--- target/linux/ar71xx/image/generic.mk
+++ target/linux/ar71xx/image/generic.mk
@@ -221,7 +221,7 @@ TARGET_DEVICES += cpe870
 
 define Device/deliberant
   BOARDNAME := DELIBERANT
-  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev rssileds
   DEVICE_TITLE := Deliberant DLB series
   MTDPARTS := spi0.0:192k(u-boot)ro,64k(u-boot-env),1152k(kernel),6528k(rootfs),1152k(kernel2),6528k(rootfs2),576k(data)ro,128k(cfg)ro,64k(art)ro,7680k@0x40000(firmware),7680k@0x7C0000(firmware2)
   IMAGE_SIZE := 7680k

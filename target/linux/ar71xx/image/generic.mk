77fd371013a565143685624687fbe6814d315823
Robert Marko <robimarko@gmail.com>
2017-09-27 00:18:05 +0200

    Syntax fix

--- target/linux/ar71xx/image/generic.mk
+++ target/linux/ar71xx/image/generic.mk
@@ -233,7 +233,8 @@ define Device/dlb-5
   MTDPARTS := spi0.0:192k(u-boot)ro,64k(u-boot-env),1152k(kernel1),6528k(rootfs1),1152k(kernel2),6528k(rootfs2),576k(data)ro,128k(cfg)ro,64k(art)ro,7680k@0x40000(firmware1),7680k@0x7C0000(firmware2),16384k@0x0(flash)
   IMAGE_SIZE := 7680k
   IMAGES := sysupgrade.bin factory.img
-  KERNEL := kernel-bin | patch-cmdline | lzma | v2v-uImage lzmaendef
+  KERNEL := kernel-bin | patch-cmdline | lzma | v2v-uImage lzma
+endef
 TARGET_DEVICES += dlb-5
 
 define Device/dragino2
